0a1154d3e60792a6888d0f9563bdcb825ec6de28
githubckgoh1439 <35713528+githubckgoh1439@users.noreply.github.com>
2018-02-08 10:51:33 +0800

    update
    done

--- students.sql
+++ students.sql
@@ -1,24 +1,6 @@
--- phpMyAdmin SQL Dump
--- version 4.0.9
--- http://www.phpmyadmin.net
---
--- Inang: localhost
--- Waktu pembuatan: 02 Mei 2014 pada 12.05
--- Versi Server: 5.5.34
--- Versi PHP: 5.4.22
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Database: nodejs
---
 
 -- --------------------------------------------------------
 
@@ -53,7 +35,3 @@ INSERT INTO students (id, address, age, emergency_contact, ic, name, phone, sex,
 
 
 
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
